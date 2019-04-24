b8b756ae44ecb329afa8ee442dcc01fc248360f3
stak21 <stakashima21@gmail.com>
2019-04-24 00:59:40 +0000

    Initial Commit: Starting web_flask; 0-hello - run and route flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_route():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
