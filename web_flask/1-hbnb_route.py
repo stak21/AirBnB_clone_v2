2e0ed6744dd146404ba860de33e2a1de665e63f7
stak21 <stakashima21@gmail.com>
2019-04-24 01:21:58 +0000

    Initial Commit: Build upon the previous task and route to /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
