f59328b7c97290ea976f193cb65d0682812c0f63
stak21 <stakashima21@gmail.com>
2019-04-24 02:02:36 +0000

    Build on the previous task and route to /number/<int:n

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    return "C {:s}".format(text).replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': "is cool"})
+@app.route('/python/<text>')
+def python_route(text):
+    return "Python {:s}".format(text).replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    return "{:d} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
