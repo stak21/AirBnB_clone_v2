01b4cca66354fced1f171c4a0cee8f99403e22a4
stak21 <stakashima21@gmail.com>
2019-04-25 04:08:12 +0000

    Initial Commit: Create a tearDown function and a route to /states_list which lists all state objects in the storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Routers and Controllers"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def tearDown(self):
+    storage.close()
+
+
+@app.route('/states_list')
+def list_states_route():
+    states = storage.all()
+    li = [ value for value in states.values() ]
+    return render_template('7-states_list.html', states=li)
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
